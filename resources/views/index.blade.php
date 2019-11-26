4509e9cf3f53eaa2448f1d516d39151e4cdbf001
ry_takahashi <ryosuke_takahashi@respawn.jp>
2019-11-26 14:53:11 +0900

    初期設定

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>{{ config('app.name') }}</title>
+</head>
+<body>
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Roboto:400">
+  <link rel="stylesheet" href="https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css">
+
+  <!-- Styles -->
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</body>
+  <div id="app"></div>
+
+
+  <!-- Scripts -->
+  <script src="{{ mix('js/app.js') }}" defer></script>
+</html>
