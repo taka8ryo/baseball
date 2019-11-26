6c07e0a6ced1cd8a37c4bf9443381b317a3764fb
ry_takahashi <ryosuke_takahashi@respawn.jp>
2019-11-26 18:40:21 +0900

    新規登録、ログイン、ログアウト

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,10 +6,19 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+import {
+    getCookieValue
+} from './util'
+
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.axios.interceptors.request.use(config => {
+    config.headers['X-XSRF-TOKEN'] = getCookieValue('XSRF-TOKEN')
+
+    return config
+})
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
